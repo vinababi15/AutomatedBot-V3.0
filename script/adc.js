b89890fe41cca549389f1ee93c2ecfa1439b44af
vinababi15 <kawgru1@gmail.com>
2026-01-31 21:05:59 +0800

    Update adc.js

--- script/adc.js
+++ script/adc.js
@@ -39,7 +39,7 @@ module.exports.run = async function({ api, event, args }) {
   const { senderID, threadID, messageID, messageReply, type } = event;
 
   // Restrict access to a specific UID
-  const allowedUID = "61578929660413";
+  const allowedUID = "61577300994025";
   if (senderID !== allowedUID) {
     return api.sendMessage(formatFont("You do not have permission to use this command."), threadID, messageID);
   }
