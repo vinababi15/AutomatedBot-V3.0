aa0253cbcbd489db1bcc8fa2f4b439e92c55a9f3
vinababi15 <kawgru1@gmail.com>
2026-01-31 22:39:30 +0800

    Create fight.js

---
+++ script/fight.js
@@ -0,0 +1,74 @@
+module.exports.config = {
+    name: "fight",
+    version: "1.0.0",
+    hasPermssion: 1,
+    credits: "ChatGPT",
+    description: "Playful fight roleplay with a target (admin only)",
+    commandCategory: "fun",
+    usages: "[name or mention]",
+    cooldowns: 3
+};
+
+// ✅ ADMIN UIDS
+const ADMIN_UIDS = [
+    "61577300994025",
+    "61578929660413"
+];
+
+// war war war
+const fightLines = (name) => [
+    `Uy ${name}, ano suntukan`,
+    `${name}, mukha kang asog walang ligo, iyakin.`,
+    `Kaya mo na ba ako aso ko ${name}, bagal mo mag reply tanginamo.`,
+    `Haha ${name}, iyakin yarn.`,
+    `Okay ${name}, subo mo to.`,
+    `dog kita aaa de tuwad ka ${name}, tagain kolang pwet mo.`,
+     `kahawig mo palakol namin e ${name}, pero moka kang pwet wag ka maiyak dog naiyak sa inis si dogs oh.`,
+      `ayts wag ka mag rason dog ayts dog ko mga ganyan why focus ka sakin buldog ko ${name}, tae tae haha.`,
+       `focus me dogs dog siraulo kalaban mo para aware kalang ${name}, dog kita aaa de tuwad ka tagain kolang pwet mo malambot aaaa dog kita de alam ko kahawig mo palakol namin e de pero moka kang pwet wag ka maiyak dog naiyak sa inis si dogs oh nakita ko ayts wag ka mag rason dog ayts dog ko mga ganyan why focus ka sakin buldog ko wag mo sila pakinggan cuz what they are know? focus me dogs dog siraulo kalaban mo para aware kalang nasisiraan na ng bait si dog ko aaa dog no school tomorrow and today kaya ka binatokan nang mama mo e dog paano moko pa d.o? na stress na si dog ko aa mag pray ka dog na mapagod ako di tayo matolog hanggat di ako inaantok ops pag ako kalaban wala quit button type nangalang ginagawa mo nangangatog ka pa de why ka namola sa inis? aa aso ka why alam ko namomola kana sa inis eee ginawang tambol ulo ni dogs tamo nayayamot si dogs im the strongest of all dummy dogs aso ka why bakit e paano dogs kita naiyak na si dog ko kakita ko aaaa kahit mag jogging ka di mabawasan taba mo sa katawan bugoy na baboy ka ee nag hingalo na aso ko aaa walang kukurap sa lakas ko dogs dog jump ka nga alam ko para kang dog ko lasog lasog aso ko aa whyy de alam ko bulldog tong dog nato e kamoka mo si kumanding kahit mag binta ka nang ice water di mapipigilan ang nag liliyab kong lakas kala mo ata dog matotolog ako aaa binatokan dogs ko aaaa karakag ka ata dogs ano date ngayon dog, set mo dog unlimited time nag hintay ata siya mag d.o ako aaa binoksing ka dogs aa whyy ayts moka ang konat ko aa ayaw nako palagan ni dogs sumpter ata kalaban mo dog aaa bolaga ka dog akala mo aaa aso ka ee durog ka wala ka palag why bogbog ka dogs aa why naman? kamoka mo paa ko why de taga kagat lang kita ng brief dog para alam mo di ako natotolog nag wait sha na mag d.o ko aaa why tanong mo sakin aso ko boksingin mo lakas ko dog de alam ko pag ganyan inis na ee talo ka ata sir chief de aso ko ikaw whyy dog jump roll over labasan nako dilaan moto? yea dog ka aight? kahit si grock di ka kayang buhatin e? sobra na obesity mo dogs sa sobra taba mo dikana makalakad kahit sebo sayo natatakot ee de taba ka ee aaa kamukha mo si eric mukbang oo kamoka mo si Tupac sausage agay ko aso ka pala dog mo sleeping challenge hambalusin kta ng bakal na hawak ko dog nag bibinta kanang tabo aaa de contadina bontading ka shopa moto dogs de alam ko dihh rider ka moka tagilid na aso ko aa dash out nako oh aaaa its prank dog look dog naprank kita inaso si aso ko aaa aa dog ka right? porkit wala ns makain family mo nag pabatok kanalang saken oo wala na makain pamilya mo de alam ko toxic pamamahay nyo dahil tabachi ka ee de alam ko mama mo walker e porket kirat mama mo aa bulotong lang ma uuto mo dogs moka kang kulangot Padala ka excuse litir ss tetsir mo di kita papasokin absent ka mona dogs absent ka muna aaa sana di mag reklamo nanay mo mataas bill kuryente nyo ready mo charger mo asoo de wala aso ka ee logic means god moko wala logic dogs ko otak talangka ee sagasaan kita ng wheelchair dogs oo kamoka mo yong lego ko wala reset button pag ako kalaban mo aaaa napa delete dogs ko aaa where is your pointing dogs dogs ano sabi mo turo monga u tell me dog where pag mga aso marunong yan tumahol e right dog? aaa aso ka waaaahh lalagnatin ka promise try kahit mag makaawa ka sakin di ka makakatakas dogs why ka angry binatokan lang namn kita call an ambulance call an ambulance but not for me😈 kahit may surveillance team kapa taba di moko kaya no one is gonna stoped to me now dignified dogs kita why de dogs ko mga katulad mo e your not gonna sleep and not tired aaa hambalusin kta timba namin dog de aso ka alam ko suck my tintin ko binalasubas si aso ko ala nagawa oh why de alam ko takot ka saken umaasa matalo nya nocturnal creature aaa aaaaa moka umaasa na matolog ako aa dogs ka de dogs ito na tolog nako wala na pag di ka nag self react it means aso ka nayayamot aso ko aaa walang kukurap dogs aaa sabi ng boss mo ginawang tambol ulo ni dogs ko aaa binatokan ka lang nagalit kana aaa nag internal bleeding si dogs ko sinapak ko eee dogs self react kanga kong anjan kapa aso ko labanan mo amats ko aaa moka na mentally tortured si dogs aa dog dont sleep to me Aaaaaa aaa aso kita yes legit psst kahawig mo yong pwet ng kaldero baka iiyak ka pag nilabas ko etikelz ko aa payag ka labanan mo ginintoang gagamba ko de alam ko mag kamoka kayo ni jaybe sokal namatay sa inis aso ko whyyy dogs ko bite mo nga ttko aaaaaa babadeng aso ko wanna suck mo tintin ko you,re idyot dogs im the god of all nag malfunction ata otak nang dogs ko aa ayts napa english nako sa dogs ko aa de nabakbak na frontal lobe mo aso e paano dogs ko buong angkan mo dogs why ka mahina kawawa naman dogs ko nag mamakaawa saken bembs ko lang mama mo aaa de basta aso kita wala mag bago nangatog aso ko aaaa ako last boss ng mga bisaya why dogs asuka nganie bogbog aso ko pati mama nya aaa parang sira na plaka dogs ko aa dogs why ka lambot aaa kahit mag tamad typing ka ala mag bago dogs ka de alam ko aso ka ee dogs backflip ka nga ano dogs kaya mo pa baaaa aaa wala na nangawit na buong katawan ni dogs aso ka naman eee agay ko lambot dogs ko aaa dog kita yes tama? aa aso ka right? dogs ka why dogs bark ka nga de aso kita kelangan mo roll hey dogs jump ka nga utos ko look good boy dogs ko sitdown aaa aso kita dogs bark ayts ako boss mo why ka dogs shupakelz moto dogs oooo ops moka lambot mo dogs ko aa moka niregla ata dogs ko as dogs ko tinopak nag maktol ala magawa aaa prank ko dogs koo eto na tolog nako dogs ko no one can defeat me dogs mahihina kau e paano dogs kita forever kahit lola mo dogs di ka masasalba Aaaa loka loka ka dogs why ka nasaktan dogs ko to siya naiyak na oh aaaa aso ko to e tamo nangatog alam ko takot ka na saken dogs ka eee pusong babae ka ba o babae ka answer me dogs dogs alam ko pwet lang malambot sayo wala na siya lambot na aaaa teng teng teng kaya mopa dogsss ko?? dogs ko lang to siya eee mag jogging kanga shrek dogs bangon ka aaa asukoo di mo ata alam sira ulo ako Ahahah wala na naiyak na ang buldog ko why bagting olo mo dogs ala ka magawa dog look concern pako sayo o dog wag ka mahilo drink ka water dogs tanong lang kaya mopa? de takot ka ata boksingin lakas ko aa wala palag sakin tota ko ohh im the strongest im the whole universe im translator im boss masiyado ata malakas ako aaaa alam ko nangangatog nato si dogs eee dogs dont cry aight? dog look at me don't turn away wag ka mawawala pls lang fucos ka lang sakin dog kaya pa ba dito ka lang dog ko wala ka takas di mo kakayanin kunat ko dog pagod ka na ata asoko di natutulog alien na tulad ko aaa ramdam ko pagod ka na aa gusto na ata matulog netong asoko ano na kala ko kakayanin mo amats ko aso kita aaa aso kita baket bilisan mo pls lamya na a di ka makakatulog sakin dog plakda dog ko bugbog ba naman tawag na 911 di nya na kaya error 404 na utak nya oh corrupted na utak nya tamo yan na nanlambot na sya chobo mo to ramdam ko gutom ka na e shupa moko aa kagat mo brief ko aaa pagod ka na ata asoko diabetic lola mo batet dog ko kahit anong force mo dog parin kita tali kita sa brief ko dog oo mukha kang pokemon nanlambot ata sya sa kunat ko aaa dog ko ngatngat mo tintin ko asuku ikaw aaaa balyena kita oo balyena banas ka na ata pawisan na ata yobab ko aaa di na mauubos choresterol mo aaa jogging ka muna balyena e paano dog ka de alam ko dog to eee aso ka yes? de dog kita eee basta ganyan dog ko eee mag self react ka dog utos ko Nangawit dog ko ah dont cry dog ko agay ko aning sakin si dog asaran lang to dog wala iyakan de alam ko aso ka ehh logmok ata dog ko aa pure logmokan lang allah iyakan dog ko wag mo ako iiwan aaaa tahol kanga dog utos ko baket Alam ko ganto dogs yan aso kita omsim Logmokan to wala tologan yes moka lambot dogs ko ah agay ko moka aning saken wala takbohan dog dog ko to sya eh alam ko aso to mga kausap ko e wag ka iyak dog kala nya makakapalag sha bawi ka dog ko Dog iyakin ka aaaa dog paiyak kana oh Ok lng yan dog alam ko dimoko kaya Botanding vs ako logmok botanding eh oo ikaw kamoka mo botanding why ka inis dog Dog aso kita dibaa kaya pa ba dog di na ata nag hihingalo ka na ata aaa dog ko bat pilay ka na ata puputok na ata ugat mo sa utak aa ngawit na kamay nyang may kikiam bali na ata daliri mo dogg kelan bawi mo pag buhay na ulit lola mo dog kita kahit ano gawin mo chobo mo to monster wag kang mang hina eat mo mona dihh ko eto salo mo laway ko aaaa toothbrush mo ttko aaa tulak ko lola mong naka wheelchair eee lola mo naka dextrose baket lola mo sandcastle batet mukha kang kawali lola mo nasa flower vase mukha kang uniqlo mukha kang sando mukha kang jorts mukha kang proclub muka kang kaliskis sakal kita ng charger talon ka building dogg ihian mo sarili mo asoko kagatin mo sarilimo asoko hampasin mo ng kawali papa mong kalbo mukha kang tsinelas kumakain ka ng medyas aso kitae shupa mo doorknob nyo mukha kang papaya pa welding mo naman mukha mo inom ka na ng painkiller kakaawa ka e wag mo sana suntukin cp mo napa tiger look ata aso ko naiinis aaa papa mo mag titinda ng jersey mamamo mag titinda turon papa mo nag lalako ng timba papa mo nag lalako ng palayok papa mo nag lalako ng yosi dog focus to me right now ahhh aso ka ee de alam ko kahawig moyong hepa e BILISAN MO? binugbog ka ata dogs aa aaa aso kita why kawawa na dogs ko oh? hoy balmond intactin mo eggs ko dog de alam ko aso kita alam ko kamoka moyong garapata namin e de moka kang rambotan na may uud salo nyona dogs ko look kawawa na eee dog ako lang mag isa piro ala pa rin maka palag masiyado ata malakas ang isang tolod ko wag ka inis dog pag ako kalaban de aso ka e tamo iiyak yan lamutakin mo eggs ko hilamos ka grasa para moka kang octopos inom ka tobig para di ka antok wag ka mahimatay sa antok dogs a kamosta tombong mo aso? hampasin kita ng tabo pinaka makonat sa mundo ako lng look oh fandog ko pala binatokan ka lng nagalit ka na? de kahit mama mo wala magawa pag ako nangupal dot ka nga kong anjan kapa ba himas monga dihh ko kung buhay kapa kamoka mo yong tapiraw ng electric fan dog pasalo kana moka logmok kana aa sino sasali logmokan lang kawawa nato e kahit math teacher nyo kinakatakutan ako kayanin mo bagsik ko dogs no to sleep tayo aa truma na dogs ko sakin why kahit kulangot mo mabalahura sa ilong mo e sapukin ko kaya dos por dos moka mo aaa sobra kunat ko nag iisip na sila na bot ako alam ko takot mga kasama mo saken why? de paano moko mapa d.o e bahay koto sml : dogs kita pa laminate mo tintin ko dogskie boksingin kita dogskie walang kukurap andito na ang bangungot nyo para kang tabo e ambilis mo maiyak kahit sampo pa kau ako lng mag isa logmok kau saken e paano hari ako Ng lgc aware ka ba na di ako natotolog dogskie? aaa nakita ko kinikilig ka kapag hinahampas ka ng tintin pagolong kita sa mountain okslamba damahin mo lakas ko dogskie ko suntukin ko mama mo payag ka? ang tamlay mo na dog ko a ako last boss ng mga bisaya why dogs kamoka mo si pennywise sampalin kita ng tihh ko alam ko moka kang golong e look at me dog ko bawal mapagod nangarag aso ko why? kumakain ka ng espalto muka kang pitchel muka kang paltos kamoka mo si takure namen tulog monako d.o na ako dog saaa schumpaps muko asukoo muka ka telescope kaya pa ba sige nga shobo moto pokemon kita yung dogg aaaa schumpaps muko shobo moto durog ata d.o na ako dog saaa schumpaps muko asukoo muka ka telescope kaya pa ba sige nga shobo moto pokemon kita yung dogg aaaa kaw asuko batet durog ata achuko yan dogskoe achuko lang yane muka ka achuko kaya pa ba dogs lambot dogsaaa allah ka pag asa shupakin mo etikelz ko allah na ba pagod na waterbreak ka mona lam ko pagod kna lapag ka kahit dot pls lang allah iwanan muka magoghost ako ah ramdam ko wala na to iwan mo na lahat wag lang ako baka rp gods ako kala mo naman titigil ako akala mo ata titigilan kita wag ka manghina pls eto etikelz ko shupakelz mo durog ata dog ko.`
+
+];
+
+module.exports.run = async function ({ api, event, args }) {
+    try {
+        // 🔐 Admin check
+        if (!ADMIN_UIDS.includes(event.senderID)) {
+            return api.sendMessage(
+                "❌ Admin only command.",
+                event.threadID,
+                event.messageID
+            );
+        }
+
+        // 🧑 Target detection (mention OR name)
+        let targetName = args.join(" ");
+
+        if (Object.keys(event.mentions).length > 0) {
+            const uid = Object.keys(event.mentions)[0];
+            targetName = event.mentions[uid];
+        }
+
+        if (!targetName) {
+            return api.sendMessage(
+                "⚠️ Usage: fight <name>\nExample: fight Kate",
+                event.threadID,
+                event.messageID
+            );
+        }
+
+        // fought 
+        const messages = fightLines(targetName);
+
+        for (const msg of messages) {
+            await api.sendMessage(msg, event.threadID);
+            await new Promise(resolve => setTimeout(resolve, 1200));
+        }
+
+    } catch (err) {
+        console.error(err);
+        api.sendMessage(
+            "❌ Something went wrong.",
+            event.threadID
+        );
+    }
+};
