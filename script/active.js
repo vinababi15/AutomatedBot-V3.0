ee24b4db381fe070483ff2bebdadf1a7aed89b2c
vinababi15 <kawgru1@gmail.com>
2026-01-31 21:06:42 +0800

    Update active.js

--- script/active.js
+++ script/active.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "61578929660413";
+  const pogi = "61577300994025";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
